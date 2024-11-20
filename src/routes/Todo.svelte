45bda3420e845a150c6f2299283999537a4004f8
Milland-2024 <amyllym1@confederationcollege.ca>
2024-11-20 16:07:22 -0500

    added item additional functionality

---
+++ src/routes/Todo.svelte
@@ -0,0 +1,26 @@
+<script>
+let todoItem = $state('');
+let todoList = $state([]);
+
+function addItem() {
+     event.preventDefault();
+     todoList.push(todoItem);
+     todoItem = '';
+}
+
+$inspect(todoList);
+
+</script>
+<form onsubmit={addItem}>
+<input type="text" bind:value={todoItem}>
+<button type="button">Add</button>
+</form>
+<ul>
+     {#each todoList as item}
+          <li>{item}</li>
+     {/each}
+</ul>
+
+<style>
+
+</style>
