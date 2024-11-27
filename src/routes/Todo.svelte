1437002e67e57393de28856ac7239b58b2a3475e
Milland-2024 <amyllym1@confederationcollege.ca>
2024-11-27 15:43:12 -0500

    initial commit

--- src/routes/Todo.svelte
+++ src/routes/Todo.svelte
@@ -1,26 +1,69 @@
 <script>
+  import { text } from "@sveltejs/kit";
+
 let todoItem = $state('');
 let todoList = $state([]);
+let doneList = $state([]);
 
-function addItem() {
+function addItem(event) {
      event.preventDefault();
-     todoList.push(todoItem);
+     if (todoItem == '') {
+          return;
+     }
+     todoList = [...todoList, {
+          text: todoItem,
+          done: false
+     }];
      todoItem = '';
 }
+function removeItem(index){
+     todoList = todoList.toSpliced(index, 1);
+}
 
+function nuke(){
+     todoList = [];
+}
+$effect (() => {
+     doneList = todoList.filter((item) => item.done);
+})
 $inspect(todoList);
+$inspect(doneList);
 
 </script>
 <form onsubmit={addItem}>
 <input type="text" bind:value={todoItem}>
-<button type="button">Add</button>
+<button type="submit">Add</button>
 </form>
+<div class="todo-list">
 <ul>
-     {#each todoList as item}
-          <li>{item}</li>
+     {#each todoList as item, index}
+          <li>
+               <input type="checkbox" bind:checked={item.done}>
+               <span class:done={item.done}>{item.text}</span>
+               <button type="button" onclick={() => removeItem(index)}>x</button>
+          </li>
      {/each}
 </ul>
+{#if (todoList.length == 0)}
+     <button disabled type="button">Delete All</button>
+{:else}
+     <button type="button" onclick={nuke}>Delete All</button>
+{/if}
+</div>
+
+{#if (doneList.length > 0)}
+<div class="done-list">
+     <h2>Done Items</h2>
+     {#each doneList as item}
+     <li>
+          {item.text}
+     </li>
+     {/each}
+</div>
+{/if}
 
 <style>
-
+ul {
+     list-style: none;
+}
 </style>
